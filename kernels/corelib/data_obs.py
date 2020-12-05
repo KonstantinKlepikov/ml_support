d3b0897b7d17357e570d9520d5fd85235f96ac81
KonstantinKlepikov <oformleno@gmail.com>
2020-12-05 22:29:25 +0200

    rebuild

--- kernels/corelib/data_obs.py
+++ kernels/corelib/data_obs.py
@@ -6,19 +6,68 @@ from zipfile import BadZipfile
 from custex import EmptyProcess
 import core_paths
 
+
+class Loader:
+
+    """Base class to data load
+    """
+
+    def __init__(self, in_path, index_col, dtype, parse_dates, sep, encoding):
+        self.in_path = in_path
+        self.index_col = index_col
+        self.dtype = dtype
+        self.parse_dates = parse_dates
+        self.sep = sep
+        self.encoding = encoding
+
+
+class CSVLoader(Loader):
+
+
+    @classmethod
+    def _is_check_for(cls, check):
+        return check == '.csv'
+
+    def loader(self):
+
+        """Loads csv to pd
+
+        Returns:
+            obj: pandas DataFrame
+        """
+
+        with open(self.in_path, 'r', encoding=self.encoding) as file_open:
+            try:
+                data_ex = pd.read_csv(file_open, sep=self.sep, index_col=self.index_col, dtype=self.dtype, parse_dates=self.parse_dates)
+            except TypeError:
+                print("'{}' is wrong name for parsing of column".format(self.index_col))
+                data_ex = None
+        
+        return data_ex
+
+
 class Processor:
 
-    def __init__(self, path_=core_paths.DATA_PATH, inslack='', outslack=''):
-        self.path_ = path_
-        self.inslack = inslack
-        self.outslack = outslack
-        self.in_path = os.path.realpath(os.path.join(path_, inslack))
-        self.out_path = os.path.realpath(os.path.join(path_, outslack))
 
     @staticmethod
     def _rjpath(path_, slack):
 
         return os.path.realpath(os.path.join(path_, slack))
+
+    @staticmethod
+    def _check_the_extention(in_path):
+
+        return os.path.splitext(in_path)[1]
+
+    @staticmethod
+    def _check_the_input(check):
+        for cls_ in Loader.__subclasses__():
+            if cls_._is_check_for(check):
+                return cls_
+        try:
+            raise ValueError
+        except ValueError:
+            print('Extention {0} is wrong! Choose another file'.format(check))
 
     @staticmethod
     def _sourcer(path_, inslack, _s_tree, _prefix=''):
@@ -42,144 +91,92 @@ class Processor:
                 Processor._sourcer(os.path.join(path_, name), inslack, _s_tree, _prefix + name + '/')
 
         return _s_tree
-
-    @staticmethod
-    def checker(in_path, dict_of_extention):
-
-        """Check data sorce
-
-        Returns:
-            obj: class exemplar for data processing for spurce file
-        """
-
-        ex = None
-        for item in dict_of_extention.items():
-            if os.path.splitext(in_path)[1] == item[0]:
-                ex = item[1]
-
-        return ex
-
-
-    def source(self):
+   
+    def source(self, path_=core_paths.DATA_PATH, inslack=''):
 
         """Print source tree
         """
 
         _s_tree = {}
-        _s_tree = self._sourcer(self.path_, self.inslack, _s_tree)
+        _s_tree = self._sourcer(path_, inslack, _s_tree)
 
         print('File .... fell path:')
         for key, val in _s_tree.items():
             print('{0} ..... {1}'.format(key, val))
 
-
-class Loader(Processor):
-
-    """Base class to data load
-    """
-
-    def __init__(self, path_=core_paths.DATA_PATH, inslack='', outslack='', index_col=None, dtype=None, parse_dates=False, sep=',', encoding=None):
-        super().__init__(path_, inslack, outslack)
-        self.index_col = index_col
-        self.dtype = dtype
-        self.parse_dates = parse_dates
-        self.sep = sep
-        self.encoding = encoding
-
-    @staticmethod
-    def _csv_load(in_path, index_col, dtype, parse_dates, sep, encoding):
-
-        """Loads csv to pd
-
-        Returns:
-            obj: pandas DataFrame
-        """
-
-        with open(in_path, 'r', encoding=encoding) as file_open:
-            try:
-                data_ex = pd.read_csv(file_open, sep=sep, index_col=index_col, dtype=dtype, parse_dates=parse_dates)
-            except TypeError:
-                print("'{}' is wrong name for parsing of column".format(index_col))
-                data_ex = None
-            
-        return data_ex
-
-    @staticmethod
-    def check_the_output(process):
-
-        if process is True:
-            return process
-        else:
-            try:
-                raise EmptyProcess
-            except EmptyProcess:
-                print('Extention of file is wrong! Choose another file')
-
-    
-    def load(self):
-
-        dict_of_extention = {'.csv': self._csv_load(self.in_path, self.index_col, self.dtype, self.parse_dates, self.sep, self.encoding)}
-        process = self.checker(self.in_path, dict_of_extention)
-
-        return self.check_the_output(process)
-
-
     def view(self):
         pass
 
+    def load(self, path_=core_paths.DATA_PATH, inslack='', index_col=None, dtype=None, parse_dates=False, sep=',', encoding=None):
 
-class CSVLoader(Loader):
+        """Read file and return pandas dataframe
+
+        path_:
+            Base placement of data files
+            string: default core_paths.DATA_PATH constant
+
+        inslack:
+            Part of file path, looks like this/that.csv
+            string: default ''
 
 
-        def _csv_load(in_path, index_col, dtype, parse_dates, sep, encoding):
+        index_col:
+            Column to use as the row labels of the DataFrame, either given as string name or column index.
+            If a sequence of int/str is given, a MultiIndex is used.
+            Note: index_col=False can be used to force pandas to not use the first column as the index, e.g. when 
+            you have a malformed file with delimiters at the end of each line.
+            int, str, sequence of int/str, or False: default None
 
-        """Loads csv to pd
+        dtype: 
+            Data type for data or columns. E.g. {‘a’: np.float64, ‘b’: np.int32, ‘c’: ‘Int64’}
+            Use str or object together with suitable na_values settings to preserve and not interpret dtype.
+            If converters are specified, they will be applied INSTEAD of dtype conversion
+            As example looks like 'dtype': {'assigned_day': np.float64}
+            dict: default None
+
+        parse_dates:
+            Parse column as datetime format
+            If True -> try parsing the index.
+            List of int or names. e.g. If [1, 2, 3] -> try parsing columns 1, 2, 3 each as a separate date column.
+            List of lists. e.g. If [[1, 3]] -> combine columns 1 and 3 and parse as a single date column.
+            Dict, e.g. {‘foo’ : [1, 3]} -> parse columns 1, 3 as date and call result ‘foo’
+            More information [pandas.read_csv](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html)
+            If a column or index cannot be represented as an array of datetimes, 
+            say because of an unparseable value or a mixture of timezones, the column 
+            or index will be returned unaltered as an object data type.
+            bool or list of int or names or list of lists or dict: default False
+
+        sep:
+            Delimiter to use
+            string: default ','
+
+        encoding: 
+            Encoding to - use for UTF when reading/writing (ex. ‘utf-8’)
+            str: default None
 
         Returns:
-            obj: pandas DataFrame
+            obj: Pandas dataFrame object
         """
 
-        with open(in_path, 'r', encoding=encoding) as file_open:
-            try:
-                data_ex = pd.read_csv(file_open, sep=sep, index_col=index_col, dtype=dtype, parse_dates=parse_dates)
-            except TypeError:
-                print("'{}' is wrong name for parsing of column".format(index_col))
-                data_ex = None
-            
-        return data_ex
-
-
-cla
-
-
-class CsvLoader(Loader):
-
-    """Load .csv files
-    """
-
-    def __init__(self, path_, inslack, index_col, dtype, parse_dates, sep, encoding):
-        super().__init__(path_, inslack, index_col, dtype, parse_dates)
-        self.sep = sep
-        self.encoding = encoding
-
-    def load(self):
-
-        """Loads csv to pd
-
-        Returns:
-            obj: pandas DataFrame
-        """
-
-        with open(self.path_, 'r', encoding=self.encoding) as file_open:
-            try:
-                data_ex = pd.read_csv(file_open, sep=self.sep, index_col=self.index_col, dtype=self.dtype, parse_dates=self.parse_dates)
-            except TypeError:
-                print("'{}' is wrong name for parsing of column".format(self.index_col))
-                data_ex = None
-            
-        return data_ex
+        in_path = self._rjpath(path_, inslack)
+        process = self._check_the_extention(in_path)
+        process = self._check_the_input(process)
+    
+        return process(in_path, index_col, dtype, parse_dates, sep, encoding).loader()
 
     def save(self):
+        pass
+
+    def pack(self):
+        pass
+
+    def unpack(self):
+        pass
+
+    def dump(self):
+        pass
+
+    def undump(self):
         pass
 
 
@@ -249,92 +246,6 @@ class ShelveDump(DataDump):
             for k, v in o.items():
                 dict_of_objects[k] = v
             return dict_of_objects.values()
-
-
-def checker(inslack, path_, dict_of_extention):
-
-    """Check data sorce
-
-    Returns:
-        obj: class exemplar for data processing for spurce file
-    """
-
-    path_ = os.path.realpath(os.path.join(path_, inslack))
-    ex = None
-    for item in dict_of_extention.items():
-        if os.path.splitext(path_)[1] == item[0]:
-            ex = item[1]
-
-    return ex
-
-def check_the_output(process):
-
-    if process:
-        return process
-    else:
-        try:
-            raise EmptyProcess
-        except EmptyProcess:
-            print('Extention of file is wrong! Choose another file')
-
-
-def loader(inslack, path_=core_paths.DATA_PATH, index_col=None, dtype=None, parse_dates=False, sep=',', encoding=None):
-
-    """Read file and return pandas dataframe
-
-    inslack:
-        Part of file path
-        string: looks like this/that.csv
-
-    path_:
-        Base placement of data files
-        string: default DATA_PATH
-
-    index_col:
-        Column to use as the row labels of the DataFrame, either given as string name or column index.
-        If a sequence of int/str is given, a MultiIndex is used.
-        Note: index_col=False can be used to force pandas to not use the first column as the index, e.g. when 
-        you have a malformed file with delimiters at the end of each line.
-        int, str, sequence of int/str, or False: default None
-
-    dtype: 
-        Data type for data or columns. E.g. {‘a’: np.float64, ‘b’: np.int32, ‘c’: ‘Int64’}
-        Use str or object together with suitable na_values settings to preserve and not interpret dtype.
-        If converters are specified, they will be applied INSTEAD of dtype conversion
-        As example looks like 'dtype': {'assigned_day': np.float64}
-        dict, default None
-
-    parse_dates:
-        Parse column as datetime format
-        If True -> try parsing the index.
-        List of int or names. e.g. If [1, 2, 3] -> try parsing columns 1, 2, 3 each as a separate date column.
-        List of lists. e.g. If [[1, 3]] -> combine columns 1 and 3 and parse as a single date column.
-        Dict, e.g. {‘foo’ : [1, 3]} -> parse columns 1, 3 as date and call result ‘foo’
-        More information [pandas.read_csv](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html)
-        If a column or index cannot be represented as an array of datetimes, 
-        say because of an unparseable value or a mixture of timezones, the column 
-        or index will be returned unaltered as an object data type.
-        bool or list of int or names or list of lists or dict: default False
-
-    sep:
-        Delimiter to use
-        string: default ','
-
-    encoding: 
-        Encoding to - use for UTF when reading/writing (ex. ‘utf-8’)
-        str: default None
-
-    Raises:
-        EmptyExtention: raised, if no one extention is available in list of readable file extentions
-
-    Returns:
-        obj: Pandas dataFrame object
-    """
-
-    dict_of_extention = {'.csv': CsvLoader(inslack, path_, index_col, dtype, parse_dates, sep, encoding)}
-    process = checker(inslack, path_, dict_of_extention)
-
-    return check_the_output(process).pd_load()
 
 
 def unpacker(inslack, path_=core_paths.DATA_PATH, outslack=''):
@@ -407,11 +318,12 @@ def dumper(dump_list=None, path_=core_paths.DUMP_PATH, inslack='default', method
 
 if __name__ == "__main__":
     
-    Processor(path_=core_paths.DATA_PATH_TEST).source()
+    pro = Processor()
+    pro.source(path_=core_paths.DATA_PATH_TEST)
     print('source.... ok')
 
-    loaded = Loader(path_=core_paths.DATA_PATH_TEST, inslack='titanic.csv').load()
-    if loaded is True:
+    loaded = pro.load(path_=core_paths.DATA_PATH_TEST, inslack='titanic.csv')
+    if isinstance(loaded, pd.DataFrame):
         print(loaded.head(1))
         print('load .... ok')
     else:
